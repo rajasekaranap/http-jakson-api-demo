26cad20970e5ae3b87014e717569e9bef5a6bf8e
Rajasekaran S <proffraja@gmail.com>
2020-03-30 20:10:47 +0530

    replaced with google http-client api

--- src/main/java/com/kgisl/raja/App.java
+++ src/main/java/com/kgisl/raja/App.java
@@ -1,6 +1,9 @@
 package com.kgisl.raja;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -10,63 +13,77 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.*;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpContent;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.json.JsonHttpContent;
+import com.google.api.client.json.GenericJson;
+import com.google.api.client.json.jackson2.JacksonFactory;
 
 public class App {
 
-    // one instance, reuse
-    private final CloseableHttpClient httpClient = HttpClients.createDefault();
-
-    public static void main(String[] args) throws Exception {
-
-        App obj = new App();
-
+    private static HttpRequestFactory REQ_FACTORY;
+    private static HttpTransport TRANSPORT;
+    private static com.google.api.client.json.JsonFactory JSON_FACTORY = new JacksonFactory();
+    public  static  void sendGet(){
+        TRANSPORT = new NetHttpTransport();
+        REQ_FACTORY = TRANSPORT.createRequestFactory();
+        GenericUrl genericUrl = new GenericUrl("https://rajaonlineportal.herokuapp.com/get/1");
+        HttpRequest httpRequest;
         try {
-            System.out.println("Testing 1 - Send Http GET request");
-            obj.sendGet();
-        } finally {
-            obj.close();
+            httpRequest = REQ_FACTORY.buildGetRequest(genericUrl);
+            HttpResponse httpResponse = httpRequest.execute();
+            String s = httpResponse.parseAsString();
+            ObjectMapper mapper = new ObjectMapper();
+            Student student = mapper.readValue(s, Student.class);
+            System.out.println(student.toString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
-    private void close() throws IOException {
-        httpClient.close();
+    public  static  void sendPost(){
+        TRANSPORT = new NetHttpTransport();
+        REQ_FACTORY = TRANSPORT.createRequestFactory();
+        GenericUrl genericUrl = new GenericUrl("https://rajaonlineportal.herokuapp.com/add");
+        Map<String,Object> data = new LinkedHashMap<>();
+        data.put("id",12);
+        data.put("name", "somename");
+        HttpContent httpContent = new JsonHttpContent(JSON_FACTORY, data);
+        try {
+            REQ_FACTORY.buildPostRequest(genericUrl, httpContent).execute();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-
-    private void sendGet() throws Exception {
-
-        HttpGet request = new HttpGet("https://rajaonlineportal.herokuapp.com/get/1");
-
-        // add request headers
-        /* request.addHeader("custom-key", "mkyong");
-        request.addHeader(HttpHeaders.USER_AGENT, "Googlebot"); */
-
-        try (CloseableHttpResponse response = httpClient.execute(request)) {
-
-            // Get HttpResponse Status
-            System.out.println(response.getStatusLine().toString());
-
-            HttpEntity entity = response.getEntity();
-            Header headers = entity.getContentType();
-            System.out.println(headers);
-
-            if (entity != null) {
-                // return it as a String
-                String result = EntityUtils.toString(entity);
-                ObjectMapper mapper = new ObjectMapper();
-                Student student = null;
-                try{
-                student = mapper.readValue(result, Student.class);
-                }
-                catch (JsonParseException e) { e.printStackTrace();}
-                catch (JsonMappingException e) { e.printStackTrace(); }
-                catch (IOException e) { e.printStackTrace(); }
-                System.out.println(result);
-                System.out.println("Student:"+student.toString());
-                System.out.println(student.getId()+100);
+    public  static  void sendGetAll(){
+        TRANSPORT = new NetHttpTransport();
+        REQ_FACTORY = TRANSPORT.createRequestFactory();
+        GenericUrl genericUrl = new GenericUrl("https://rajaonlineportal.herokuapp.com/get/all");
+        HttpRequest httpRequest;
+        try {
+            httpRequest = REQ_FACTORY.buildGetRequest(genericUrl);
+            HttpResponse httpResponse = httpRequest.execute();
+            String s = httpResponse.parseAsString();
+            ObjectMapper mapper = new ObjectMapper();
+            Student[] students = mapper.readValue(s, Student[].class);
+            for(Student x:students){
+                System.out.println(x.toString());
             }
-
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-
     }
-
+    public static void main(String[] args) {
+        //sendGet();
+        //sendPost();
+        sendGetAll();
+    }
 }
